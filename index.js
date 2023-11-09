6657d67a71e1d2108dacd60ab975a85b62528245
Madhur Saxena <saxenamadhur96@gmail.com>
2023-11-10 03:47:31 +0530

    Config

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ const urlRoute = require('./api/urlRoute');
 const { connectToDB } = require('./config/dbConfig');
 require('dotenv').config();
 const app = express();
-const PORT = 4000;
 connectToDB(process.env.MONGODB_URL).then(() => {
     console.log('DB Connected');
 });
 app.use(express.json());
 app.use('/', urlRoute);
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT, () => {
     console.log('Server running');
 })
